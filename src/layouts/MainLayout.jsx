4b771c5004303efb010a57bb4866394498f3092d
anderu <andrewtan1310@gmail.com>
2024-04-29 12:50:17 +0800

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function MainLayout() {
   return (
-    <div className='h-screen bg-bg-second'>
+    <div className='h-screen max-w-[400px] mx-auto bg-bg-second'>
       <Outlet />
     </div>
   );
